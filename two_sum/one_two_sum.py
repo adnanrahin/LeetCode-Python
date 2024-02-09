e70b54b3b93c50beefbe14dbffa38c45550a793f
adnanrahin <adnan.rahin13@gmail.com>
2024-02-09 17:11:15 -0500

    two_sum

---
+++ two_sum/one_two_sum.py
@@ -0,0 +1,19 @@
+from _ast import List
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        duplicates = {}
+        for i in range(len(nums)):
+            sum = target - nums[i]
+            if sum in duplicates:
+                return {i, duplicates[sum]}
+            else:
+                duplicates[nums[i]] = i
+
+        return {0, 0}
+
+
+nums = [2, 7, 11, 15]
+print(Solution().twoSum(nums, 9))
+print(Solution().twoSum([3,3], 6))
